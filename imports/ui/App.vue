a5c40a05811724459e8378318f536c4156683798
virza805 <virza805@gmail.com>
2020-09-14 10:40:15 +0600

    Starting to integrate vue support

---
+++ imports/ui/App.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <p class="text-color">
+            {{ greeting }}
+        </p>
+
+        <temp-component/>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                greeting: 'Hello Meteor, from Vue',
+            };
+        },
+    };
+</script>
+
+<style lang="css" scoped>
+.text-color{
+    color:rgb(2, 128, 19);
+}
+</style>
